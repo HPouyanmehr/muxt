8a162cf44941deef59dbe25cff3d4a0f708e580e
Hosein <a.h.pouyanmehr@gmail.com>
2021-12-20 14:03:43 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+import '../styles/globals.css';
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
